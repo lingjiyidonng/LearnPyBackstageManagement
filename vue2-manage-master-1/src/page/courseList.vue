6121f5faea628cd05d653f03b6a2d1a964842c38
caijingjing <1966226779@qq.com>
2021-11-13 10:40:35 +0800

    add courseList

---
+++ vue2-manage-master-1/src/page/courseList.vue
@@ -0,0 +1,185 @@
+<template>
+    <div class="fillcontain">
+        <head-top></head-top>
+        <div class="table_container">
+            <el-table
+                :data="tableData"
+                highlight-current-row
+                style="width: 100%">
+                <!-- <el-table-column
+                  type="index"
+                  width="100">
+                </el-table-column> -->
+                <el-table-column
+                  label="阶段"
+                  type="index"
+                  width="100">
+                </el-table-column>
+                <el-table-column
+                  property="username"
+                  label="内容"
+                  width="220">
+                </el-table-column>
+                <el-table-column
+                  property="telephonenumber"
+                  label="详情"
+                  width="220">
+                </el-table-column>
+                <el-table-column label="操作" width="160">
+                  <template slot-scope="scope">
+                    <el-button
+                      size="small"
+                      @click="handleEdit(scope.row)">编辑</el-button>
+                    <el-button
+                      size="small"
+                      type="danger"
+                      @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                  </template>
+                </el-table-column>
+                <!-- <el-table-column
+                  property="registe_time"
+                  label="注册日期"
+                  width="220">
+                </el-table-column>
+                <el-table-column
+                  property="username"
+                  label="用户姓名"
+                  width="220">
+                </el-table-column>
+                <el-table-column
+                  property="city"
+                  label="注册地址">
+                </el-table-column> -->
+            </el-table>
+            <div class="Pagination" style="text-align: left;margin-top: 10px;">
+                <el-pagination
+                  @size-change="handleSizeChange"
+                  @current-change="handleCurrentChange"
+                  :current-page="currentPage"
+                  :page-size="20"
+                  layout="total, prev, pager, next"
+                  :total="count">
+                </el-pagination>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import headTop from '../components/headTop'
+    import {getUserList, getUserCount} from '@/api/getData'
+    export default {
+        data(){
+            return {
+                tableData: [{
+                  registe_time: '2016-05-02',
+                  username: '王小虎',
+                  city: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                  registe_time: '2016-05-04',
+                  username: '王小虎',
+                  city: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                  registe_time: '2016-05-01',
+                  username: '王小虎',
+                  city: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                  registe_time: '2016-05-03',
+                  username: '王小虎',
+                  city: '上海市普陀区金沙江路 1516 弄'
+                }],
+                currentRow: null,
+                offset: 0,
+                limit: 20,
+                count: 0,
+                currentPage: 1,
+            }
+        },
+    	components: {
+    		headTop,
+    	},
+        created(){
+            this.initData();
+        },
+        methods: {
+            async initData(){
+                try{
+                    const countData = await getUserCount();
+                    if (countData.status == 1) {
+                        this.count = countData.count;
+                    }else{
+                        throw new Error('获取数据失败');
+                    }
+                    this.getUsers();
+                }catch(err){
+                    console.log('获取数据失败', err);
+                }
+            },
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                this.currentPage = val;
+                this.offset = (val - 1)*this.limit;
+                this.getUsers()
+            },
+            async getUsers(){
+                const Users = await getUserList({offset: this.offset, limit: this.limit});
+                this.tableData = [];
+                Users.forEach(item => {
+                    const tableData = {};
+                    tableData.username = item.username;
+                    tableData.registe_time = item.registe_time;
+                    tableData.city = item.city;
+                    this.tableData.push(tableData);
+                })
+            }
+        },
+    }
+</script>
+<style lang="less">
+	@import '../style/mixin';
+    .demo-table-expand {
+        font-size: 0;
+    }
+    .demo-table-expand label {
+        width: 90px;
+        color: #99a9bf;
+    }
+    .demo-table-expand .el-form-item {
+        margin-right: 0;
+        margin-bottom: 0;
+        width: 50%;
+    }
+    .table_container{
+        padding: 20px;
+    }
+    .Pagination{
+        display: flex;
+        justify-content: flex-start;
+        margin-top: 8px;
+    }
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+        border-color: #20a0ff;
+    }
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 120px;
+        height: 120px;
+        line-height: 120px;
+        text-align: center;
+    }
+    .avatar {
+        width: 120px;
+        height: 120px;
+        display: block;
+    }
+</style>
